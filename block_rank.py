9b09542cfffd0ea0bed78aea7cf85d66b7971789
Philip Hinch <phinch@brown.edu>
2016-08-04 22:26:13 -0400

    BlockRank
    Initial analysis of most appealing blocks

---
+++ block_rank.py
@@ -0,0 +1,122 @@
+import json
+import math
+import operator
+import collections
+import csv
+
+all_blocks = {}
+with open("all_blocks.json", 'r') as data:
+    all_blocks = json.load(data)
+
+"""
+JSON Format:
+{
+    "40.xxx -73.xxx": {
+        avg_passengers: float,
+        total_passengers: int,
+        destinations: {block: int, block: int, ...},
+        avg_tip: float,
+        avg_distance: float,
+        total_distance: float,
+        total_tip: float,
+        ride_count: int,
+        avg_revenue: float,
+        total_revenue: float
+    }, ...
+}
+"""
+
+#This algorithm is an initial attempt at exploring blocks by their level of appeal.
+#At the outset, this means just sorting them by average or total revenue, but this obviously doesn't tell the whole story.
+#Another important dimension is an appealing destination; I want a block where I'm likely to travel to another appealing block, to continue the cycle
+#We will iteratively sort the blocks by appeal until we reach some level of convergence, i.e. a consistent ordering of blocks. 
+
+#First, we want to sort the blocks into a list; we'll make 2 lists, one for average and one for total revenue.
+#For fun, we'll also make a sort based on the total number of rides. 
+#In the first iteration, this makes the most sense in terms of appeal, because outliers dominate revenue.
+block_list = []
+
+for block in all_blocks:
+    block_list += [(block, all_blocks[block])]
+
+size = len(block_list)
+
+avg_order = sorted(block_list, key=lambda d: d[1]['avg_revenue'])
+total_order = sorted(block_list, key=lambda d: d[1]['total_revenue'])
+ride_order = sorted(block_list, key=lambda d: d[1]['ride_count'])
+
+#We'll run the ranking algorithm based on avg_order first.
+#The general path of the algorithm:
+#For each block:
+#   score = 0
+#   For each destination:
+#       score += destination_frequency * destination_score (where score = 1-index in the list)
+#   Store total score
+#Order the blocks based on new scores
+#Find number of changes between the previous ordering and this ordering
+#If within a certain threshhold:
+#   break
+#Else:
+#   repeat
+
+#We can stop once the list stops changing its order; we might have some threshhold above 0 to account for very close scores
+convergence = 0.01
+
+old_blocks = []
+new_scores = []
+new_blocks = []
+
+for item in avg_order:
+    old_blocks.append(item[0])
+
+converged = False
+
+iter_count = 0
+while not converged:
+    old_dict = {}
+    block_score = 0
+    for block in old_blocks:
+        block_score += 1
+        old_dict[block] = block_score
+
+    iter_count += 1
+    for block in block_list:
+
+        destinations = block[1]['destinations']
+        score = 0
+        for dest in destinations:
+            try:
+                score += math.sqrt(destinations[dest])*(old_dict[dest]) 
+            except KeyError: #Not all pickup blocks are destinations; these cases are discounted as completely unappealing
+                continue
+        
+        new_scores.append((block[0], score))
+    
+    new_scores.sort(key=lambda d: d[1])
+    
+    for item in new_scores:
+        new_blocks.append(item[0])
+
+    differences = 0
+    for i in range(0, len(new_blocks)):
+        if(new_blocks[i] != old_blocks[i]):
+            differences += 1        
+
+    print(differences, iter_count)
+
+    old_blocks = new_blocks
+    new_blocks = []
+    new_scores = []
+
+    if differences < size*convergence or iter_count > 500:
+        break
+
+#old_blocks now contains the final ordering, iterated to convergence
+
+with open("avg_revenue_rank_2.csv", 'w', newline = '') as output:
+    writer = csv.writer(output)
+    writer.writerow(["Block"])
+    for item in old_blocks:
+        writer.writerow([item])
+
+
---
+++ divide_blocks.py
@@ -0,0 +1,97 @@
+import json
+import requests
+import math
+import csv
+import datetime
+
+#Format of CSV
+"""
+['VendorID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime', 'passenger_count', 'trip_distance', 'pickup_longitude', 'pickup_latitude', 'RateCodeID', 'store_and_fwd_flag', 'dropoff_longitude', 'dropoff_latitude', 'payment_type', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount', 'total_amount']
+"""
+
+#In this script, the raw data CSV will be divided into "blocks," with all data being aggregated without respect to time
+
+#This data includes total revenue, total tip, total rides, total passengers, total distance; averages of all of these; and a JSON object tracking destinations
+
+# open the csv
+raw_data = csv.reader(open('../yellow_tripdata_2015-01-06.csv', 'r'), delimiter = ",")
+# skip the header
+next(raw_data, None)
+
+#Variables for the indices of the csv, for readability of code. These are the indices we want in the sorted csvs.
+passengers = 3
+distance = 4
+pickup_long = 5
+pickup_lat = 6
+dropoff_long = 9
+dropoff_lat = 10
+payment_type = 11
+tip = 15
+total = 17
+
+count = 0
+
+all_blocks = {}
+
+#Change this to vary the size of the "block." size=1 is approx. 1 sq. block (slightly larger)
+size = 5
+
+def getBlock(lat, lng):
+    #Takes a lat and a long, as string objects, and rounds to return the block as described above
+    return str(round(round(float(lat)/size, 3)*size, 3))+" "+str(round(round(float(lng)/size, 3)*size, 3))
+
+for ride in raw_data:
+    count += 1
+    print(count) #To track progress instead of staring at a blank screen
+
+    if(float(ride[pickup_lat]) == 0 or float(ride[pickup_long]) == 0 or float(ride[total]) <= 0 or float(ride[dropoff_lat]) == 0 or float(ride[dropoff_long]) == 0):
+        continue
+
+    block = getBlock(ride[pickup_lat], ride[pickup_long])
+    toblock = getBlock(ride[dropoff_lat], ride[dropoff_long])
+    
+    #ride_count, total_revenue, total_tip, total_distance, total_passengers, tip_rides, destinations
+    if block in all_blocks:
+        all_blocks[block]['ride_count'] += 1
+        all_blocks[block]['total_revenue'] += float(ride[total])
+        all_blocks[block]['total_tip'] += float(ride[tip])
+        all_blocks[block]['total_distance'] += float(ride[distance])
+        all_blocks[block]['total_passengers'] += int(ride[passengers])
+        if ride[payment_type] == "1":
+            all_blocks[block]['tip_rides'] += 1
+        if toblock in all_blocks[block]['destinations']:
+            all_blocks[block]['destinations'][toblock] += 1
+        else:
+            all_blocks[block]['destinations'][toblock] = 1
+    else:
+        all_blocks[block] = {}
+        all_blocks[block]['ride_count'] = 1
+        all_blocks[block]['total_revenue'] = float(ride[total])
+        all_blocks[block]['total_tip'] = float(ride[tip])
+        all_blocks[block]['total_distance'] = float(ride[distance])
+        all_blocks[block]['total_passengers'] = int(ride[passengers])
+        all_blocks[block]['tip_rides'] = 0
+        if ride[payment_type] == "1":
+            all_blocks[block]['tip_rides'] = 1
+        all_blocks[block]['destinations'] = {}
+        all_blocks[block]['destinations'][toblock] = 1
+    
+for b in all_blocks:
+    block = all_blocks[b]
+    #averages: revenue, tip, distance, passengers
+    block['avg_revenue'] = round(block['total_revenue']/block['ride_count'], 2)
+    if block['tip_rides'] > 0:
+        block['avg_tip'] = round(block['total_tip']/block['tip_rides'], 2)
+    else:
+        block['avg_tip'] = "N/A"
+    block['avg_distance'] = round(block['total_distance']/block['ride_count'], 2)
+    block['avg_passengers'] = round(block['total_passengers']/block['ride_count'], 2)
+    block['total_revenue'] = round(block['total_revenue'], 2)
+    block['total_tip'] = round(block['total_tip'], 2)
+    block['total_distance'] = round(block['total_distance'], 2)
+
+with open("all_blocks.json", 'w') as output:
+    json.dump(all_blocks, output)
+        
+            
+
